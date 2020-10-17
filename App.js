7ab64d9918c203e4cb4da524230fa5a3f94146ac
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2020-10-17 10:38:54 -0300

    Add camera bug example

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const cameraRef = useRef();
+  const [photo, setPhoto] = useState();
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  async function capture() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync({ exif: true });
+      console.log(photo);
+      setPhoto(photo);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.flex}>
+      <View style={styles.row}>
+        <Camera ref={cameraRef} style={styles.flex} />
+        <View style={styles.flex}>
+          {photo && <Image style={styles.flex} source={{ uri: photo.uri }} />}
+        </View>
+      </View>
+      <Button title="Capture" onPress={capture} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  row: {
+    flexDirection: 'row',
+    height: '50%',
+  },
+  flex: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
